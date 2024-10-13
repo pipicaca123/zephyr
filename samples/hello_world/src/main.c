a344822efffb72c58d077bf1f6ad3e053cf27972
yicheng.lou <yicheng.lou@tronfuture.com>
2024-10-13 18:24:29 +0800

    uart_pl intr test pass

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -15,11 +15,12 @@
 #define GPIO_PIN_0 0
 
 #define UART0_DEVICE_NODE DT_NODELABEL(uart0)
-#define UARTLITE_DEVICE_NODE DT_NODELABEL(uartlite0)
+#define PL_UART_DEVICE_NODE DT_NODELABEL(uart16550_0)
+// #define PL_UART_DEVICE_NODE DT_NODELABEL(uartlite0)
 #define MSG_SIZE 32
 K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
 static const struct device *const uart0_dev = DEVICE_DT_GET(UART0_DEVICE_NODE);
-static const struct device *const uartlite0_dev = DEVICE_DT_GET(UARTLITE_DEVICE_NODE);
+static const struct device *const pl_uart_dev = DEVICE_DT_GET(PL_UART_DEVICE_NODE);
 static int rx_buf_pos;
 
 void serial_cb(const struct device *dev, void *user_data)
@@ -36,30 +37,31 @@ void serial_cb(const struct device *dev, void *user_data)
 		printf("%c",c);
 	}
 }
-void serial_lite_cb(const struct device *dev, void *user_data)
-{
-	uint8_t c;
-	if (!uart_irq_update(uartlite0_dev)) {
-		return;
-	}
-	if (!uart_irq_rx_ready(uartlite0_dev)) {
-		return;
-	}
-	/* read until FIFO empty */
-	print_uart("echo: ");
-	while (uart_fifo_read(uartlite0_dev, &c, 1) == 1) {
-		print_uart("%c",c);
-	}
-	print_uart("\r\n");
-}
-void print_uart(char *buf)
+static void print_pl_uart(char *buf)
 {
 	int msg_len = strlen(buf);
 
 	for (int i = 0; i < msg_len; i++) {
-		uart_poll_out(uartlite0_dev, buf[i]);
+		uart_poll_out(pl_uart_dev, buf[i]);
 	}
 }
+void serial_lite_cb(const struct device *dev, void *user_data)
+{
+	uint8_t c;
+	if (!uart_irq_update(pl_uart_dev)) {
+		return;
+	}
+	if (!uart_irq_rx_ready(pl_uart_dev)) {
+		return;
+	}
+	/* read until FIFO empty */
+	print_pl_uart("echo: ");
+	while (uart_fifo_read(pl_uart_dev, &c, 1) == 1) {
+		uart_poll_out(pl_uart_dev, c);
+	}
+	print_pl_uart("\r\n");
+}
+
 
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
@@ -79,12 +81,12 @@ int main(void)
 	gpio_leds_dev = device_get_binding("axi_gpio@41210000");
 	if (gpio_leds_dev == NULL) {
 		printf("Failed to bind to AXI GPIO device\n");
-		return;
+		return 0;
 	}
 	ret = gpio_pin_configure(gpio_leds_dev, GPIO_PIN_0, GPIO_OUTPUT);
 	if (ret != 0) {
 		printf("Failed to configure GPIO pin %d\n", GPIO_PIN_0);
-		return;
+		return 0;
 	}
 	ret = gpio_pin_set(gpio_leds_dev, GPIO_PIN_0, 1);
 	if (ret != 0) {
@@ -94,12 +96,12 @@ int main(void)
 	gpio_btns_dev = device_get_binding("axi_gpio@41200000");
 	if (gpio_btns_dev == NULL) {
 		printf("Failed to bind to AXI GPIO device\n");
-		return;
+		return 0;
 	}
 	ret = gpio_pin_configure(gpio_btns_dev, GPIO_PIN_0, GPIO_INPUT);
 	if (ret != 0) {
 		printf("Failed to configure GPIO pin %d\n", GPIO_PIN_0);
-		return;
+		return 0;
 	}
 	ret = gpio_pin_interrupt_configure(gpio_btns_dev,GPIO_PIN_0, GPIO_INT_EDGE_RISING);
 	if (ret != 0) {
@@ -127,12 +129,12 @@ int main(void)
 	}
 	uart_irq_rx_enable(uart0_dev);
 
-	//uartlite
-	if (!device_is_ready(uartlite0_dev)) {
+	//pl uart
+	if (!device_is_ready(pl_uart_dev)) {
 		printf("UART device not found!");
 		return 0;
 	}
-	ret = uart_irq_callback_user_data_set(uartlite0_dev, serial_lite_cb, NULL);
+	ret = uart_irq_callback_user_data_set(pl_uart_dev, serial_lite_cb, NULL);
 	if (ret < 0) {
 		if (ret == -ENOTSUP) {
 			printf("Interrupt-driven UART API support not enabled\n");
@@ -143,19 +145,18 @@ int main(void)
 		}
 		return 0;
 	}
-	uart_irq_rx_enable(uartlite0_dev);
-	print_uart("Hello! I'm your echo bot.\r\n");
-	print_uart("Tell me something and press enter:\r\n");
-
+	uart_irq_rx_enable(pl_uart_dev);
+	print_pl_uart("\r\nHello! I'm your echo bot.\r\n");
+	print_pl_uart("Tell me something and press enter:\r\n");
 	// app
 	for (int i = 0; i < 20; i++) {
-		int val = 0;
-		// printf("toggle btn = 0x%x\r\n", val);
 		gpio_pin_toggle(gpio_leds_dev, GPIO_PIN_0);
 		k_msleep(500);
 	}
 	printf("test ended.\r\n");
 
-	while(1){}
+	while(1){
+		// do something
+	}
 	return 0;
 }
